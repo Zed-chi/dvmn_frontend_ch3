ee8845ea7dbad0d7309a5b3cac1b474985dfe9ee
alex <zed-chi@mail.ru>
2020-09-21 13:22:44 +0300

    black fix

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://www.franksonnenbergonline.com/blog/are-you-grateful/'
+url = "https://www.franksonnenbergonline.com/blog/are-you-grateful/"
 response = requests.get(url)
 response.raise_for_status()
 print(response.text)
-soup = BeautifulSoup(response.text, 'lxml')
+soup = BeautifulSoup(response.text, "lxml")
 print(soup.prettify())
-title_tag = soup.find('main').find('header').find('h1')
+title_tag = soup.find("main").find("header").find("h1")
 title_text = title_tag.text
-soup.find('img', class_='attachment-post-image')['src']
+soup.find("img", class_="attachment-post-image")["src"]
 post = sopu.find("div", class_="entry-content")
 text = post.text
