2796b1f286cc10e49405cc2219c440e753bf8418
zed <zed@mail.ru>
2020-09-24 19:43:59 +0300

    link parser

---
+++ parse_tululu_category.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+import urllib
+
+"#content > .d_book > tbody > tr:first-child > td:first-child a"
+SFICTION_URL = "http://tululu.org/l55/"
+
+
+def get_first_link(html):
+    soup = BeautifulSoup(html, "lxml")
+    href = (
+        soup.find("div", id="content")
+        .find(class_="d_book")
+        .find("a")
+        .get("href")
+    )
+    link = urllib.parse.urljoin(SFICTION_URL, href)
+    return link
+
+
+def get_url_content(url):
+    res = requests.get(SFICTION_URL)
+    res.raise_for_status()
+    return res.text
+
+
+if __name__ == "__main__":
+    html = get_url_content(SFICTION_URL)
+    print(get_first_link(html))
