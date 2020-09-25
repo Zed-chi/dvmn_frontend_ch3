6a9a703cb1672b8f7086c79a068999211007cf3e
zed-chi <zed-chi@mail.ru>
2020-09-25 13:07:31 +0300

    naming fix

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from utils import *
+from parse_tululu_category import (get_all_book_links_on_page, get_sfiction_list_books_page)
 
 
 BASE_URL = "http://tululu.org"
@@ -16,8 +17,8 @@ def download_10_books():
 def download_txt_from_tululu_by_id(id):
     book_url = f"{BOOK_URL}{id}"
     book_details_page = f"{BASE_BOOK_PAGE}{id}"
-    txt = get_url_content(book_url)
-    html = get_url_content(book_details_page, allow_redirects=True)
+    txt = get_text_from_url(book_url)
+    html = get_text_from_url(book_details_page, allow_redirects=True)
     info = get_book_details(html)
     if info and txt:
         filename = f"{id}.{info['title']}.txt"
@@ -29,7 +30,7 @@ def download_txt_from_tululu_by_id(id):
 def get_images_from_10_books():
     for id in range(1, 11):
         book_details_page = f"{BASE_BOOK_PAGE}{id}"
-        html = get_url_content(book_details_page, allow_redirects=True)
+        html = get_text_from_url(book_details_page, allow_redirects=True)
         info = get_book_details(html)
         if info and info["img_url"] is not None:
             try:
@@ -46,16 +47,17 @@ def get_images_from_10_books():
                 print(e)
 
 
-def download_100_books():
-    books_path = os.path.join("./", "/books")
+def download_100_books():    
+    books_path = os.path.join("./", "books")
     images_path = os.path.join("./", "images")
     links = []
-    description = {}
+    description = []
     for id in range(1, 2):
         html = get_sfiction_list_books_page(id)
         links.extend(get_all_book_links_on_page(html))
+        print(f"Chunk {id} processed")
     for id, link in enumerate(links):
-        html = get_url_content(link, allow_redirects=True)
+        html = get_text_from_url(link, allow_redirects=True)
         info = get_book_details(html)
         if "nopic" in info["img_url"]:
             name = "nopic"
@@ -63,14 +65,17 @@ def download_100_books():
             name = f"{id}"
         if requests.head(info["img_url"]).status_code == 200:
             ext = info["img_url"].split(".")[-1]
-            filename = f"{name}.{ext}"
-            info["img_src"] = os.path.join(images_path, filename)
-            info["book_path"] = os.path.join(books_path, filename)            
+            image_filename = f"{name}.{ext}"
+            book_filename = f"{id}.{info['title']}.txt"
+            info["img_src"] = os.path.join(images_path, image_filename)
+            info["book_path"] = os.path.join(books_path, book_filename)
             book_id = get_id_from_book_url(link)
-            txt_link = f"{BOOK_URL}{book_id}"            
+            txt_link = f"{BOOK_URL}{book_id}"
             download_image(from_=info["img_url"], to=info["img_src"])
             download_txt(from_=txt_link, to=info["book_path"])
-            description.update(info)
+            description.append(info)
+        print(f"Book {id} saved")
+    make_description({"books": description})
 
 
 if __name__ == "__main__":
