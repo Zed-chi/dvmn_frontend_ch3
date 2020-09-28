43c1b29bb81179cce3bfc9c73525f2b94e9452aa
zed <zed@mail.ru>
2020-09-28 21:12:53 +0300

    some naming fixes

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from parse_tululu_category import (
 
 BASE_URL = "http://tululu.org"
 BASE_BOOK_PAGE = "http://tululu.org/b"
-BOOK_URL = "http://tululu.org/txt.php?id="
+BASE_TXT_URL = "http://tululu.org/txt.php?id="
 
 
 def get_args():
@@ -39,32 +39,38 @@ def get_name_from_url_or_id(url, id):
 
 def main():
     args = get_args()
-    books_path = os.path.join(args.dest_folder, "books")
-    images_path = os.path.join(args.dest_folder, "images")
+    ###
+    books_dir = os.path.join(args.dest_folder, "books")
+    images_dir = os.path.join(args.dest_folder, "images")
     json_filepath = args.json_path or os.path.join(
         args.dest_folder, "books.json"
     )
+    ###
     links = get_links_from_pages(args.start_page, args.end_page)
     description = []
     for id, link in enumerate(links):
-        html = get_text_from_url(link, allow_redirects=True)
-        info = get_book_details(html)
-        if not args.skip_imgs:
-            image_filename = get_name_from_url_or_id(info["img_url"], id)
-            info["img_src"] = os.path.normcase(
-                os.path.join(images_path, image_filename)
-            )
-            download_image(from_=info["img_url"], to=info["img_src"])
-        if not args.skip_txt:
-            book_filename = f"{id}.{info['title']}.txt"
-            info["book_path"] = os.path.normcase(
-                os.path.join(books_path, book_filename)
-            )
-            book_id = get_id_from_book_url(link)
-            txt_link = f"{BOOK_URL}{book_id}"
-            download_txt(from_=txt_link, to=info["book_path"])
-        description.append(info)
-
+        try:
+            html = get_text_from_url(link, allow_redirects=True)
+            details = get_book_details(html)
+            if not args.skip_imgs:
+                image_filename = get_name_from_url_or_id(
+                    details["img_url"], id
+                )
+                details["img_src"] = os.path.normcase(
+                    os.path.join(images_dir, image_filename)
+                )
+                download_image(from_=details["img_url"], to=details["img_src"])
+            if not args.skip_txt:
+                book_filename = f"{id}.{details['title']}.txt"
+                details["book_path"] = os.path.normcase(
+                    os.path.join(books_dir, book_filename)
+                )
+                txt_id = get_id_from_book_url(link)
+                txt_link = f"{BASE_TXT_URL}{txt_id}"
+                download_txt(from_=txt_link, to=details["book_path"])
+            description.append(details)
+        except Exception as e:
+            print(e)
     make_description({"books": description}, json_filepath)
 
 
